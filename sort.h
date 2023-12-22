1aac52299544deb2a8b8966d9f049de140b32a24
iDamina <daminailiya@gmail.com>
2023-12-22 13:44:12 +0100

    quick sort

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /*MAIN_H*/
